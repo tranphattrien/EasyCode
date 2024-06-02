3bef6dd32b1efd5a8d863993ebb6bed33ae5c121
Tran Phat Trien <trientranphat@gmail.com>
2024-06-02 18:50:17 +0700

    search-user

---
+++ src/components/usercard.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function UserCard({ user }) {
+  const {
+    personal_info: { fullname, username, profile_img }
+  } = user;
+  return (
+    <Link to={`/user/${username}`} className="flex gap-5 items-center mb-5">
+      <img
+        src={profile_img}
+        alt="Avatar of user"
+        className="w-14 h-14 rounded-full"
+      />
+      <div>
+        <h1 className="font-medium text-xl line-clamp-2">{fullname}</h1>
+        <p className="text-dark-grey">@{username}</p>
+      </div>
+    </Link>
+  );
+}
--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -8,9 +8,11 @@ import NoDataMessage from "../components/nodata.component";
 import LoadMoreDataBtn from "../components/load-more.component";
 import axios from "axios";
 import { filterPaginationData } from "../common/filter-pagination-data";
+import UserCard from "../components/usercard.component";
 export default function SearchPage() {
   let { query } = useParams();
   const [blogs, setBlogs] = useState(null);
+  const [users, setUsers] = useState(null);
   const searchBlogs = async ({ page = 1, create_new_arr = false }) => {
     try {
       const response = await axios.post(
@@ -30,12 +32,51 @@ export default function SearchPage() {
       console.log(error);
     }
   };
+
+  const searchUsers = async () => {
+    try {
+      const response = await axios.post(
+        import.meta.env.VITE_SERVER_DOMAIN + "/search-users",
+        { query }
+      );
+      setUsers(response.data.users);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     resetState();
     searchBlogs({ page: 1, create_new_arr: true });
+    searchUsers();
   }, [query]);
+
   const resetState = () => {
     setBlogs(null);
+    setUsers(null);
+  };
+
+  const UserCardWrapper = () => {
+    return (
+      <>
+        {users === null ? (
+          <Loader />
+        ) : users.length ? (
+          users.map((user, i) => {
+            return (
+              <AnimationWrapper
+                key={i}
+                transition={{ duration: 1, delay: i * 0.08 }}
+              >
+                <UserCard user={user} />
+              </AnimationWrapper>
+            );
+          })
+        ) : (
+          <NoDataMessage message="No user found" />
+        )}
+      </>
+    );
   };
   return (
     <section className="h-cover flex justify-center gap-10">
@@ -66,7 +107,15 @@ export default function SearchPage() {
             )}
             <LoadMoreDataBtn state={blogs} fetchDataFunction={searchBlogs} />
           </>
+
+          <UserCardWrapper />
         </InpageNavigation>
+      </div>
+      <div className="min-w-[40%] lg:min-w-[350px] max-w-min border-1 border-grey pl-8 pt-3 max-md:hidden">
+        <h1 className="font-medium text-xl mb-8">
+          <i className="fi fi-rr-user mt-1" /> User related to search
+        </h1>
+        <UserCardWrapper />
       </div>
     </section>
   );
